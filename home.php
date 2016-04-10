a1ef0829bcbe5074b73d549fe29e7c64ee3fec0d
Mojtaba <mojtabadarvishi@gmail.com>
2016-04-10 19:09:51 +0430

    Home's post elements completed

--- home.php
+++ home.php
@@ -83,7 +83,8 @@
 			<div class="row">
 				<div class="col-xs-8">
 					<div class="home_content">
-						<div class="box style_1">
+
+						<div class="box">
 							<div class="title">
 								<a href="#" class="more pull-right">More</a>
 								<h4>Sport</h4>
@@ -129,7 +130,7 @@
 							</div><!-- body -->
 						</div><!-- box -->
 
-						<div class="box style_2">
+						<div class="box">
 							<div class="title">
 								<a href="#" class="more pull-right">More</a>
 								<h4>Shop</h4>
@@ -181,7 +182,7 @@
 							</div><!-- body -->
 						</div><!-- box -->
 
-						<div class="box style_3">
+						<div class="box">
 							<div class="title">
 								<a href="#" class="more pull-right">More</a>
 								<h4>Technology</h4>
@@ -216,6 +217,120 @@
 										</div>
 
 									</div>
+								</div><!-- row -->
+							</div><!-- body -->
+						</div><!-- box -->
+
+						<div class="box">
+							<div class="title">
+								<a href="#" class="more pull-right">More</a>
+								<h4>Technology</h4>
+								<h6>Defeated Panthers to get back</h6>
+							</div><!-- title -->
+							<div class="body">
+								<div class="row">
+									<div class="col-xs-12">
+										<div class="big_post horizontal">
+											<ul>
+												<li>
+													<div class="thumb"><img src="assets/img/post-01-215-215.jpg" height="75" width="100" alt=""></div>
+													<h2><a href="#">The world is changed! i feel in the water, i feel in the air</a></h2>
+													<div class="time"><i class="fa fa-clock-o"></i> Friday, 27 April 2016</div>
+													<div class="excerpt">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci</div>
+												</li>
+												<li>
+													<div class="thumb"><img src="assets/img/post-02-215-215.jpg" height="75" width="100" alt=""></div>
+													<h2><a href="#">The world is changed! i feel in the water, i feel in ...</a></h2>
+													<div class="time"><i class="fa fa-clock-o"></i> Friday, 27 April 2016</div>
+													<div class="excerpt">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci</div>
+												</li>
+											</ul>
+										</div>
+
+									</div>
+								</div><!-- row -->
+							</div><!-- body -->
+						</div><!-- box -->
+
+						<div class="box">
+							<div class="title">
+								<a href="#" class="more pull-right">More</a>
+								<h4>Technology</h4>
+								<h6>Defeated Panthers to get back</h6>
+							</div><!-- title -->
+							<div class="body">
+								<div class="row">
+									<div class="col-xs-12">
+										<div class="small_post">
+											<ul>
+												<li>
+													<div class="thumb"><img src="assets/img/top_new_01.jpg" height="75" width="100" alt=""></div>
+													<h2><a href="#">The world is changed! i feel in the water, i feel in ...</a></h2>
+													<div class="time"><i class="fa fa-clock-o"></i> Friday, 27 April 2016</div>
+												</li>
+												<li>
+													<div class="thumb"><img src="assets/img/top_new_02.jpg" height="75" width="100" alt=""></div>
+													<h2><a href="#">The world is changed! i feel in the water, i feel in ...</a></h2>
+													<div class="time"><i class="fa fa-clock-o"></i> Friday, 27 April 2016</div>
+												</li>
+												<li>
+													<div class="thumb"><img src="assets/img/top_new_03.jpg" height="75" width="100" alt=""></div>
+													<h2><a href="#">The world is changed! i feel in the water, i feel in ...</a></h2>
+													<div class="time"><i class="fa fa-clock-o"></i> Friday, 27 April 2016</div>
+												</li>
+												<li>
+													<div class="thumb"><img src="assets/img/top_new_04.jpg" height="75" width="100" alt=""></div>
+													<h2><a href="#">Why You Should Avoid Sass @extend</a></h2>
+													<div class="time"><i class="fa fa-clock-o"></i> Friday, 27 April 2016</div>
+												</li>
+											</ul>
+										</div>
+
+									</div>
+								</div><!-- row -->
+							</div><!-- body -->
+						</div><!-- box -->
+
+						<div class="box">
+							<div class="title">
+								<a href="#" class="more pull-right">More</a>
+								<h4>Sport</h4>
+								<h6>Defeated Panthers to get back</h6>
+							</div><!-- title -->
+							<div class="body">
+								<div class="row">
+									<div class="col-xs-6">
+										<div class="big_post">
+											<div class="thumb"><img src="assets/img/post_01.jpg" height="250" width="420" alt=""></div>
+											<h2><a href="#">The world is changed! i feel in the water, i feel in ...</a></h2>
+											<div class="time"><i class="fa fa-clock-o"></i> Friday, 27 April 2016</div>
+											<div class="excerpt">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat ut laoreet dolore magna aliquam erat volutpat ut laoreet dolore magna  aliquam erat volutpat.</div>
+										</div><!-- big_post -->
+									</div><!-- col-xs-6 -->
+									<div class="col-xs-6">
+										<div class="big_post">
+											<div class="thumb"><img src="assets/img/post_01.jpg" height="250" width="420" alt=""></div>
+											<h2><a href="#">The world is changed! i feel in the water, i feel in ...</a></h2>
+											<div class="time"><i class="fa fa-clock-o"></i> Friday, 27 April 2016</div>
+											<div class="excerpt">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat ut laoreet dolore magna aliquam erat volutpat ut laoreet dolore magna  aliquam erat volutpat.</div>
+										</div><!-- big_post -->
+									</div><!-- col-xs-6 -->
+									<div class="col-xs-6">
+										<div class="big_post">
+											<div class="thumb"><img src="assets/img/post_01.jpg" height="250" width="420" alt=""></div>
+											<h2><a href="#">The world is changed! i feel in the water, i feel in ...</a></h2>
+											<div class="time"><i class="fa fa-clock-o"></i> Friday, 27 April 2016</div>
+											<div class="excerpt">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat ut laoreet dolore magna aliquam erat volutpat ut laoreet dolore magna  aliquam erat volutpat.</div>
+										</div><!-- big_post -->
+									</div><!-- col-xs-6 -->
+									<div class="col-xs-6">
+										<div class="big_post">
+											<div class="thumb"><img src="assets/img/post_01.jpg" height="250" width="420" alt=""></div>
+											<h2><a href="#">The world is changed! i feel in the water, i feel in ...</a></h2>
+											<div class="time"><i class="fa fa-clock-o"></i> Friday, 27 April 2016</div>
+											<div class="excerpt">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat ut laoreet dolore magna aliquam erat volutpat ut laoreet dolore magna  aliquam erat volutpat.</div>
+										</div><!-- big_post -->
+									</div><!-- col-xs-6 -->
 								</div><!-- row -->
 							</div><!-- body -->
 						</div><!-- box -->
