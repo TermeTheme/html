7c86438e8876421b661d9598a0b207e7b6a66d08
nimamamany007 <nima.rahimi@live.com>
2016-05-17 13:18:22 +0430

    Customizable Footer File

--- home.php
+++ home.php
@@ -537,6 +537,7 @@
 			</div><!-- row -->
 		</div><!-- container -->
 	</main>
+<?php include 'templates/footer/3.php'; ?>
 	<script type="text/javascript" src="assets/js/jquery.min.js"></script>
 	<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="assets/js/typed.js"></script>
